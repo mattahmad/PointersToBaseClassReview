acb2d9fcbcb441b4c303454cc5470bb95a13c116
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-11 20:33:05 -0400

    Pointers to Base Class Review

--- PointersToBaseClassReview/main.cpp
+++ PointersToBaseClassReview/main.cpp
@@ -1,14 +1,36 @@
-//
-//  main.cpp
-//  PointersToBaseClassReview
-//
-//  Created by Matt Ahmad on 2022-10-11.
-//
-
 #include <iostream>
+using namespace std;
+
+class Polygon {
+protected:
+    int width, height;
+public:
+    void set_values (int a, int b)
+    { width=a; height=b; }
+};
+
+class Rectangle: public Polygon {
+public:
+    int area()
+    { return width*height; }
+};
+
+class Triangle: public Polygon {
+public:
+    int area()
+    { return width*height/2; }
+};
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Rectangle rect;
+    Triangle trgl;
+    Polygon * ppoly1 = &rect;
+    Polygon * ppoly2 = &trgl;
+    ppoly1->set_values (4, 5);
+    ppoly2->set_values (4, 5);
+    cout << rect.area() << '\n';
+    cout << trgl.area() << '\n';
     return 0;
 }
